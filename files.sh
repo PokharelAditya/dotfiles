eb3bcc61b83e89ca1ee2cc1025b4323aa5bf1bc6
Aditya Pokharel <pokhareladitya.pro@gmail.com>
2026-08-09 22:45:11 +0545

    files.sh

--- files.sh
+++ files.sh
@@ -5,6 +5,7 @@ PRIVATE_HOME_FILES=(
     ".ssh"
     ".config/opencode"
     ".config/omniroute"
+    ".config/sunshine"
 )
 
 PRIVATE_SYSTEM_FILES=(
@@ -16,15 +17,32 @@ PRIVATE_SYSTEM_FILES=(
 # Each entry: "<src-relative-to-HOME> <dest-folder-in-dotfiles/home>"
 # ─────────────────────────────────────────
 PUBLIC_HOME_FILES=(
-    ".config/hypr          hypr"
-    ".zshrc                zsh"
-    ".p10k.zsh             zsh"
-    ".gitconfig            git"
-    ".local/bin            scripts"
-    "wallpapers            wallpapers"
+    ".config/discover_overlay/config.ini      discord"
+    ".config/gtk-3.0/settings.ini             gtk"
+    ".config/gtk-4.0/settings.ini             gtk"
+    ".config/hypr                             hypr"
+    ".config/i3                               i3"
+    ".config/kitty                            kitty"
+    ".config/nvim                             nvim"
+    ".config/qt5ct                            qt"
+    ".config/qt6ct                            qt"
+    ".config/rog                              rog"
+    ".config/waybar                           waybar"
+    ".config/brave-flags.conf                 brave"
+    ".config/mimeapps.list                    desktop"
+    ".zshrc                                   zsh"
+    ".p10k.zsh                                zsh"
+    ".gitconfig                               git"
+    ".local/bin                               scripts"
+    "wallpapers                               wallpapers"
 )
 
 # Each entry: "<src-relative-to-SYSTEM>"
 PUBLIC_SYSTEM_FILES=(
-    # e.g. "/etc/some-public-config"
+    "/etc/default/grub"
+    "/boot/grub/themes"
+    "/etc/sddm.conf"
+    "/usr/share/sddm"
+    "/var/lib/AccountsService"
+    "/etc/pacman.conf"
 )
